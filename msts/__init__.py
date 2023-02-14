eb81dd46b027f3fe73efb93161dafe2bebd78e85
Argmaster <argmaster.world@gmail.com>
2023-02-15 00:02:27 +0100

    Update versions of hooks to match those from pyproject file

---
+++ msts/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Krzysztof Wiśniewski <argmaster.world@gmail.com>
+#
+# This file is part of MSTS.
+#
+#
+# MSTS is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# MSTS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with MSTS. If not, see <http://www.gnu.org/licenses/>.
+
+"""Minecraft Status Tracking Server - HTTP web application for tracking status of
+minecraft server."""
+
+from __future__ import annotations
+
+__version__ = "1.0.0"
