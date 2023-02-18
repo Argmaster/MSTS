8c3a62deef7a0d517aebd7c743b27e87e5f28315
Argmaster <argmaster.world@gmail.com>
2023-02-19 00:49:17 +0100

    Add eslint and typescript hooks

--- frontend/src/cookie.tsx
+++ frontend/src/cookie.tsx
@@ -1,8 +1,8 @@
 export function getCookie(name: string) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
         while (c.charAt(0) == " ") c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) == 0)
             return c.substring(nameEQ.length, c.length);
@@ -10,10 +10,10 @@ export function getCookie(name: string) {
     return null;
 }
 
-export function setCookie(name: string, value: any, minutes: number) {
-    var expires = "";
+export function setCookie(name: string, value: string, minutes: number) {
+    let expires = "";
     if (minutes) {
-        var date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + minutes * 60 * 1000);
         expires = "; expires=" + date.toUTCString();
     }
